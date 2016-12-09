335c9c4ca630c528f6eb17cb2b94a510dc687ae5
Avsky <m.wolcz96@gmail.com>
2016-12-09 22:55:12 +0100

    Sort-of-initial commit

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Python script for **Q**uickly **As**se**m**bling and **L**inking programs.
 
 Simple script eliminating the need to manually run nasm and gcc/ld every time you want to rebuild your simple asm program.
+
+# Usage
+```
+-h   --help           Print this help.  
+-k   --keep-object    Don't remove object file after successful linking.  
+-r   --autorun        Automatically run the program after successful linking.  
+-c   --compiler       Select alternative compiler. Defaults to nasm.  
+-l   --linker         Select alternative linker. Defaults to ld.  
+-t   --temp-dir       Change where to save the object file if you wish to keep it.  
+```
---
+++ qasml.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+import getopt
+import os
+import platform
+import subprocess
+import sys
+import tempfile
+
+
+def usage():
+    print("Usage: ", end="", flush=True)
+    print(os.path.basename(sys.argv[0]) + " [-hkr] [-c COMPILER] [-l LINKER] [-t TEMPDIRECTORY] INPUTFILE OUTPUTFILE")
+    print("\t-h\t--help\t\t\tPrint this help.\n\t-k\t--keep-object\t\tDon't remove object file after successful linking.\n\t-r\t--autorun\t\tAutomatically run the program after successful linking.\n\t-c\t--compiler\t\tSelect alternative compiler. Defaults to nasm.\n\t-l\t--linker\t\tSelect alternative linker. Defaults to ld.\n\t-t\t--temp-dir\t\tChange where to save the object file if you wish to keep it.")
+
+
+def main(argv):
+    asmFile = ''
+    asmHandler = 'nasm'
+    asmLinker = 'ld'
+    tmpDir = tempfile.gettempdir()
+    removeObjAfterCompilation = True
+    outputFile = ''
+    runAfterwards = False
+    arch = None
+    if platform.machine() == "x86_64":
+        arch = "elf64"
+    else:
+        arch = "elf32"
+
+    try:
+        opts, args = getopt.gnu_getopt(argv, "hkl:c:t:r",
+                                       ["help", "keep-object", "linker=", "compiler=", "temp-dir=", "autorun"])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+        elif o in ("-k", "--keep-object"):
+            removeObjAfterCompilation = False
+        elif o in ("-l", "--linker"):
+            asmLinker = a
+        elif o in ("-c", "--compiler"):
+            asmHandler = a
+        elif o in ("-t", "--temp-dir"):
+            if os.path.isdir(a):
+                tmpDir = a
+            else:
+                print("{0} is not a directory!".format(a))
+                sys.exit(2)
+        elif o in ("-r", "--autorun"):
+            runAfterwards = True
+        else:
+            assert False, "unhandled option"
+
+    if len(args) != 2:
+        print("Incorrect amount of arguments: " + str(args))
+        usage()
+    else:
+        asmFile = args[0]
+        outputFile = args[1]
+        if os.path.isfile(asmFile):
+            objectPath = os.path.abspath(tmpDir) + "/qasml-" + os.path.basename(outputFile) + ".o"
+            # print("Compiling {0} with {1}, linking to {2} with {3}.".format(asmFile, asmHandler, outputFile, asmLinker))
+            buildCommand = [asmHandler, asmFile, "-f", arch, "-o", objectPath]
+            print("Running: " + " ".join(buildCommand))
+
+            if subprocess.call(buildCommand) != 0:
+                print("\nSomething went wrong, you're on your own.")
+                sys.exit(127)
+            print("OK!")
+
+            linkCommand = [asmLinker, objectPath, "-o", outputFile]
+            print("Running: " + " ".join(linkCommand))
+            if subprocess.call(linkCommand) != 0:
+                print("\nSomething went wrong, you're on your own.")
+                os.remove(objectPath)
+                sys.exit(127)
+            print("OK!")
+
+            if removeObjAfterCompilation:
+                print("Cleaning up " + objectPath)
+                os.remove(objectPath)
+
+            if runAfterwards:
+                print("Running: " + outputFile)
+                if subprocess.call([os.path.abspath(outputFile)]) != 0:
+                    print("\nSomething appears to have gone wrong, don't care at this point.")
+
+        else:
+            print("{0} doesn't seem to exist.".format(asmFile))
+            sys.exit(2)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
